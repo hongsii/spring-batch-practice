6b6c1cfa2ce42c4451edb264186b891147a774c0
hongsii <hongsii.dev@gmail.com>
2020-06-12 01:59:32 +0900

    프로젝트 생성

---
+++ src/main/java/dev/hongsii/springbatchpractice/SpringBatchPracticeApplication.java
@@ -0,0 +1,13 @@
+package dev.hongsii.springbatchpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/hongsii/springbatchpractice/SpringBatchPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hongsii.springbatchpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
